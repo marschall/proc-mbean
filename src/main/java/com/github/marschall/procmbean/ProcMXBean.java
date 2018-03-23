5ddb8887576789f5dceea5b7b8521832d7ba743c
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-23 07:52:32 +0100

    add more files

--- src/main/java/com/github/marschall/procmbean/ProcMXBean.java
+++ src/main/java/com/github/marschall/procmbean/ProcMXBean.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public interface ProcMXBean {
 
+  // grep -i pagesize /proc/self/smaps
+
+  String smaps();
+
+  String stat();
+
+  String statm();
+
+  String status();
+
   List<Mapping> getMappings();
 
   int getOomScore();
